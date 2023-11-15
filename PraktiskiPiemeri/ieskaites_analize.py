7836743efa372d97c85ae35e73492563745ccb1a
VVSprogramm <74134370+VVSprogramm@users.noreply.github.com>
2023-11-15 13:16:13 +0200

    Create ieskaites_analize.py

---
+++ PraktiskiPiemeri/ieskaites_analize.py
@@ -0,0 +1,19 @@
+"""
+Dota bibliotēka, izvadi elementu - 52
+
+ 
+
+Dots:
+
+dati = {'Grupa': {'A': {'Jānis':[60,65,23], 'Pēteris':[68,56,25], 'Viesturs':[65,62,18]}, 'B': {'Lūkass':[58,64,20], 'Daniels':[61,52,24], 'Visvaris':[67,8,28]}}}
+
+"""
+
+dati = {'9.klase': {'A': {'Jānis':[60,65,23], 'Pēteris':[68,56,25], 'Viesturs':[65,62,18]}, 
+                  'B': {'Lūkass':[58,64,20], 'Daniels':[61,52,24], 'Visvaris':[67,8,28]}},
+        '10.kl':'vērtība'}
+
+print("52")
+print('Daniels'[1])
+print('9.klase','B','Daniels'[1])
+print(dati['9.klase']['B']['Daniels'][1])
