812f6fd2527786bf864647467afe7f1df30a32a3
VVSprogramm <74134370+VVSprogramm@users.noreply.github.com>
2023-12-07 15:50:23 +0200

    Create treninam_07_12.py

---
+++ treninam_07_12.py
@@ -0,0 +1,6 @@
+"""
+Uzraksti funkciju, kura atgriež vērtību True, ja abi dotie vārdi sākas ar vienu burtu.
+
+print(sakas_vienadi("Liela lama")) ------> True
+print(sakas_vienadi("Maza Lama")) ------> False
+"""
